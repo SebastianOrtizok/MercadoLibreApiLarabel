4468db206ace6847370681aacee0f128fc7a2126
“Sebastian <impactotenisok@gmail.com>
2025-04-15 18:20:08 -0300

    Excluir /register de CSRF temporalmente para pruebas

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/login', // Excluye la ruta del login temporalmente.
+        '/login',
+        '/register', // Excluye temporalmente para probar
     ];
 
 }
