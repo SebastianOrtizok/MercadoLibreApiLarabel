05345ed46b76c4a22c2ee4c34e5284fb911490c2
“Sebastian <impactotenisok@gmail.com>
2025-04-15 15:28:28 -0300

    Forzar HTTPS en config/app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'url' => env('APP_URL', 'https://mercadolibreapi.onrender.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://mercadolibreapi.onrender.com'),
 
     /*
     |--------------------------------------------------------------------------
