b64bfc63a4462b4f082e80f9c2df47bcec290f6e
“Sebastian <impactotenisok@gmail.com>
2025-04-15 18:16:24 -0300

    Limpiar caché al arrancar en entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Extensiones cargadas:"
 php -m
-php artisan migrate:reset --force
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
 php scripts/run-migrations.php
 php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
