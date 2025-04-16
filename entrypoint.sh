d389934242b01d670bcab9116de46b09362a7cbd
“Sebastian <impactotenisok@gmail.com>
2025-04-16 10:38:17 -0300

    entryponit con clear cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 echo "Extensiones cargadas:"
 php -m
 # Opcional: limpiar caché solo si es necesario
-# php artisan cache:clear
-# php artisan config:clear
-# php artisan route:clear
+ php artisan cache:clear
+ php artisan config:clear
+ php artisan route:clear
 # No ejecutamos migraciones para evitar pérdida de datos
 # php scripts/run-migrations.php
 php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
