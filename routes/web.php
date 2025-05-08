7d039e11a1c00acfd2c0c3744b73820870cff96d
“Sebastian <impactotenisok@gmail.com>
2025-05-08 19:22:11 -0300

    denug base de datos

--- routes/web.php
+++ routes/web.php
@@ -121,3 +121,11 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
+
+
+use Illuminate\Support\Facades\DB;
+
+Route::get('/debug-table-structure', function () {
+    $structure = DB::select('DESCRIBE items_competidores');
+    return response()->json($structure);
+});
